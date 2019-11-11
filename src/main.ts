4f0f5f5aadfa117620085805c1a12fdb91803d46
patzick <tomczyk.patryk@gmail.com>
2019-11-11 17:11:41 +0100

    test : load refs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 import { wait } from "./wait";
+import axios from "axios";
 
 async function run() {
   try {
@@ -8,6 +9,15 @@ async function run() {
     console.log(`Waiting ${ms} milliseconds ...`);
     console.log(`PR: ${getPrNumber()}`);
     console.log(`CONTEXT`, github.context);
+
+    const address =
+      github.context &&
+      github.context.payload &&
+      github.context.payload.repository &&
+      github.context.payload.repository.git_refs_url;
+
+    const refsResponse = await axios.get(address);
+    console.log("REFS", refsResponse.data);
 
     core.debug(new Date().toTimeString());
     await wait(parseInt(ms, 10));
