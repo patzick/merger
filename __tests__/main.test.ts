13ef3c81c0d009984bd62c5a3848726927a26b36
patzick <tomczyk.patryk@gmail.com>
2019-11-11 13:40:21 +0100

    test: github context

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -19,7 +19,7 @@ test('wait 500 ms', async() => {
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
     process.env['INPUT_MILLISECONDS'] = '500';
-    const ip = path.join(__dirname, '..', 'lib', 'main.js');
+    const ip = path.join(__dirname, '..', 'dist', 'index.js');
     const options: cp.ExecSyncOptions = {
         env: process.env
     };
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,31 @@
-import * as core from '@actions/core';
-import {wait} from './wait'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { wait } from "./wait";
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    const ms = core.getInput("milliseconds");
+    console.log(`Waiting ${ms} milliseconds ...`);
+    console.log(`PR: ${getPrNumber()}`);
+    console.log(`CONTEXT`, github.context);
 
-    core.debug((new Date()).toTimeString())
+    core.debug(new Date().toTimeString());
     await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
+    core.debug(new Date().toTimeString());
 
-    core.setOutput('time', new Date().toTimeString());
+    core.setOutput("time", new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
 run();
+
+function getPrNumber(): number | undefined {
+  const pullRequest = github.context.payload.pull_request;
+  if (!pullRequest) {
+    return undefined;
+  }
+
+  return pullRequest.number;
+}
